5a8ffb49069ac5b795dfede8f55839a25be3353f
Farleir <farleir@gmail.com>
2025-06-25 22:55:15 -0300

    index.js

---
+++ pick2me-app/src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css'; // Importa o CSS principal (onde pode ter as diretivas do Tailwind)
+import App from './App';
+
+// Cria a raiz da aplicação React, apontando para o elemento com id 'root' no seu index.html
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+// Renderiza o componente principal 'App' dentro da raiz
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
